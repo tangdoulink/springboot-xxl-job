c8773ee17d97e5a154c44c539f57cd03d8cfb026
tangdou <tangdoulink@163.com>
2023-02-04 17:58:35 +0800

    init project

---
+++ src/main/java/com/zpb/SpringbootXxlJob.java
@@ -0,0 +1,18 @@
+package com.zpb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author       pengbo.zhao
+ * @description  springboot-xxl-job-启动类
+ * @createDate   2021/11/8 15:58
+ * @updateDate   2021/11/8 15:58
+ * @version      1.0
+ */
+@SpringBootApplication
+public class SpringbootXxlJob {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootXxlJob.class);
+    }
+}
---
+++ src/main/java/com/zpb/config/XxlJobConfig.java
@@ -0,0 +1,55 @@
+package com.zpb.config;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author       pengbo.zhao
+ * @description  xxl-job-配置
+ * @createDate   2021/11/8 15:42
+ * @updateDate   2021/11/8 15:42
+ * @version      1.0
+ */
+@Configuration
+public class XxlJobConfig {
+
+    @Value("${xxl.job.admin.addresses}")
+    private String adminAddresses;
+
+    @Value("${xxl.job.accessToken}")
+    private String accessToken;
+
+    @Value("${xxl.job.executor.appname}")
+    private String appname;
+
+    @Value("${xxl.job.executor.address}")
+    private String address;
+
+    @Value("${xxl.job.executor.ip}")
+    private String ip;
+
+    @Value("${xxl.job.executor.port}")
+    private int port;
+
+    @Value("${xxl.job.executor.logpath}")
+    private String logPath;
+
+    @Value("${xxl.job.executor.logretentiondays}")
+    private int logRetentionDays;
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        System.err.println(">>>>>>>>>>> xxl-job config init.");
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAppname(appname);
+        xxlJobSpringExecutor.setIp(ip);
+        xxlJobSpringExecutor.setPort(port);
+        xxlJobSpringExecutor.setAccessToken(accessToken);
+        xxlJobSpringExecutor.setLogPath(logPath);
+        xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
+        return xxlJobSpringExecutor;
+    }
+}
